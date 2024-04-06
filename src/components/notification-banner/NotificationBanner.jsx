f17952fa4b8a219506600a3063a7016be07f0bab
Santosh A <santu0002@gmail.com>
2024-04-07 00:47:32 +0530

    notify banner update

--- src/components/notification-banner/NotificationBanner.jsx
+++ src/components/notification-banner/NotificationBanner.jsx
@@ -4,13 +4,13 @@ const NotificationBanner = () => {
   return (
     <div className="bg-dark text-white py-3 flex justify-center items-center fixed left-0 w-full z-50 bottom-0 top-auto md:top-0 md:bottom-auto">
       <div className="container w-full max-w-4xl mx-auto px-6 flex justify-between items-center flex-col md:flex-row">
-        <p className="m-0 text-center mb-2">Notification banner content goes here that can be changed accordingly</p>{" "}
+        <p className="m-0 text-center mb-2">Binary prediction dapp is now live on ton blockchain mainnet</p>{" "}
         <a
-          href="https://airtable.com/appwA2iT5p19G03FM/shrrlJAwFVR8ZwpZO"
+          href="https://t.me/wagmi11predictbot/predict"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <button className="primary-btn light small">Invest Now</button>
+          <button className="primary-btn light small">Predict & Win Now</button>
         </a>
       </div>
     </div>
